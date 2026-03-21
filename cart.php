44643cd1b8fbe63bd6e637dfcba77a6cf4b4a452
Amen Allah <amanallah.jendoubi@gmail.com>
2026-03-21 19:08:59 +0100

    making cart page responsive

--- cart.php
+++ cart.php
@@ -1,108 +1,82 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+        theme: {
+        extend: {
+            screens: {
+            'mouse': {'raw': '(hover: hover)'},
+            }
+        }
+        }
+    }
+    </script>
+
     <title>Cart</title>
 </head>
+
+
 <body>
-    <header>
-        <p class="ad">Winter Sale And Free Express Delivery - OFF 50%! <a href="some link to shop">shop Now !</a></p> <!--think about auto traduction-->
-        
-        <div class="nav-container">
-            <h1>Exclusive</h1>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>About</li>
-                    <li>Sign Up</li>
-                </ul>
-            </nav>
-            <div class="search">
-                <input type="text" id="search" name="search" placeholder="What are you looking for?">
-                <img src="resources/Vector.png" alt="heart">
-                <img src="resources/Cart1 with buy.png" alt="shop">
+    <?php 
+    include 'header.php';
+    ?>
+    <main class="w-[80%] mx-auto bg-slate-50">
+        <div class="flex flex-col">
+            <div class="flex justify-between font-semibold bg-white shadow-lg p-4 mb-10">
+                <h3 class="w-[20%]">Product</h3>
+                <h3 class="w-[20%]">Price</h3>
+                <h3 class="w-[20%]">Quantity</h3>
+                <h3 class="w-[20%]">Subtotal</h3>
+            </div>
+            <div class="flex justify-between items-center bg-white shadow-lg p-4">
+                <div class="flex flex-col w-[20%]">
+                    <img class="block ml-3" src="./resources/lcd-monitor.jpg" alt="">
+                    <p class="text-center">LCD Monitor</p>
+                </div>
+                <p class="w-[20%]">$650</p>
+                <div class="w-[20%]"><input class="max-w-[100%] ring-2 ring-orange-500 outline-none" type="number" min="1" value="1"></div>
+                <p class="w-[20%]">$650</p>
             </div>
         </div>
-    </header>
-    <main>
-        <section class="cart-menu">
-            <div class="row">
-                <h1>Product</h1>
-                <h1>Price</h1>
-                <h1>Quantity</h1>
-                <h1>Subtotal</h1>
+        <div class="w-[60%] mx-auto border border-black bg-white mt-20 p-6 rounded-lg">
+            <h3 class="text-center font-semibold text-xl mb-1">Cart Total</h3>
+            <div class="bordeer border-b-2 border-gray-300 flex justify-between">
+                <p>Subtotal</p>
+                <p>$650</p>
+            </div>
+            <div class="bordeer border-b-2 border-gray-300 flex justify-between">
+                <p>Shipping</p>
+                <p>Free</p>
+            </div>
+            <div class="flex justify-between text-red-700 font-medium">
+                <p>Total</p>
+                <p>$650</p>
+            </div>
+            <div class="flex justify-center items-center mt-10">
+                <button class="bg-orange-600 hover:bg-[#DB4444] p-3 px-5 rounded-full text-sm text-white md:text-base">Create Account</button>
+            </div>
+        </div>
+    
 
-            </div>
-            <div class="row">
-                <p>products</p>
-                <p>$650</p>
-                <input type="number" min="0" value="0">
-                <p>$650</p>
-            </div>
-            <div class="row">
-                <button type="button">Return To Shop</button>
-                <button type="button">Update Cart</button>
-            </div>
-        </section>
-        <section class="checkout-prep">
-            <div class="row">
-                <div>
-                    <input type="text" placeholder="Coupon Code">
-                    <button type="button">Apply Coupon</button>
-                </div>
-                <div>
-                    <h1>Cart Total</h1>
-                    <button type="button"><a href="checkout.html">Procees to checkout</a></button>
-                </div>
-            </div>
-        </section>
+
+
+
+
+
+
+
+
+
+
+
+
     </main>
 
-    <footer>
-        <div>
-            <h1>Exclusive</h1>
-            <p>Subscribe</p>
-            <p>Get 10% off your first order</p>
-            <input type="text" placeholder="Enter your email">
-        </div>
-        <div>
-            <h1>Support</h1>
-            <p>111 Bijoy sarani, Dhaka,  DH 1515, Bangladesh.</p>
-            <p>exclusive@gmail.com</p>
-            <p>+88015-88888-9999</p>
-        </div>
-        <div>
-            <h1>Account</h1>
-            <p>My Account</p>
-            <p>Login / Register</p>
-            <p>Cart</p>
-            <p>Wishlist</p>
-            <p>Shop</p>
-        </div>
-        <div>
-            <h1>Quick Link</h1>
-            <p><a href="">Privacy</a></p>
-            <p><a href="">Terms Of Use</a></p>
-            <p><a href="">FAQ</a></p>
-            <p><a href="">Contact</a></p>
-        </div>
-        <div>
-            <h1>Download App</h1>
-            <div class="download-container">
-                <img src="resources/Qrcode 1.png" alt="">
-                <div>
-                    <a href=""><img src="resources/png-transparent-google-play-store-logo-google-play-app-store-android-wallets-text-label-logo.png" alt=""></a><a href=""><img src="resources/download-appstore.png" alt=""></a>
-                </div>
-            </div>
-            <div class="socials">
-                <a href=""><img src="resources/Vector (5).png" alt="Facebook"></a>
-                <a href=""><img src="resources/Group.png" alt="Instagram"></a>
-                <a href=""><img src="resources/Vector (7).png" alt="LinkedIn"></a>
-            </div>
-        </div>
-    </footer>
+<?php 
+include 'footer.php';
+?> 
 </body>
 </html>
