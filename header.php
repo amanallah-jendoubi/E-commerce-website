e98382a5b72a78329182b2d0f73e44338cd1e1f5
Amen Allah <amanallah.jendoubi@gmail.com>
2026-03-21 12:31:47 +0100

    Switching to php

---
+++ header.php
@@ -0,0 +1,52 @@
+    <header class="mb-12">
+        <p class="bg-black  text-slate-200 py-6 font-mono text-base text-center">
+        Winter Sale And Free Express Delivery
+        <span class="block font-semibold sm:inline " > OFF 50%!</span>
+        </p>       
+        <div class="nav-container w-[90%] mx-auto">
+            <div class="flex justify-between">
+                <h2 class="font-bold text-3xl my-3 md:pt-1 lg:pt-1 lg:text-4xl">Exclusive</h2>
+                <nav class="my-3 hidden md:block">
+                    <ul class="flex justify-center">
+                        <li class="m-4   mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">Home</li>
+                        <li class="m-4  mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">Contact</li>
+                        <li class="m-4  mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">Sign Up</li>
+                        <li class="m-4  mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">About</li>
+                    </ul>
+                </nav>
+                <label class="relative sm:flex sm:items-center sm: hidden w-[40%] md:w-[30%] lg:w-[40%]">
+                    <img class="absolute left-7 lg:left-10 top-1/2 -translate-y-1/2 w-5 h-5 object-contain" src="./resources/search.png" alt="search icon">
+                    <input class=" bg-gray-100 focus:outline-none focus:ring-1 focus:ring-black shadow-md rounded-full p-2 pl-11 mx-auto w-[90%]" type="text" id="search" name="search" placeholder="Search">
+                </label>
+                <div class="flex justify-center items-center">
+                    <img src="resources/Cart1 with buy.png" alt="shop">
+                </div>
+            </div>
+            <label class="relative block sm:hidden mx-auto w-[90%]">
+                <img class="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 object-contain" src="./resources/search.png" alt="search icon">
+                <input class=" bg-gray-100 focus:outline-none focus:ring-1 focus:ring-black shadow-md rounded-full p-2 pl-11 mx-auto w-[90%]" type="text" id="search" name="search" placeholder="Search">
+            </label>
+            <nav class="my-3 md:hidden">
+                <ul class="flex justify-center">
+                    <li class="m-4 lg:m-6 lg:text-lg  mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">Home</li>
+                    <li class="m-4 lg:m-6 lg:text-lg mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">Contact</li>
+                    <li class="m-4 lg:m-6 lg:text-lg mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">Sign Up</li>
+                    <li class="m-4 lg:m-6 lg:text-lg  mouse:hover:text-red-500 mouse:hover:underline mouse:hover:underline-offset-4">About</li>
+                </ul>
+            </nav>
+        </div>
+        <section class="hidden">
+            <nav>
+                <ul>
+                    <li><a href="">Men's Fashion</a></li>
+                    <li><a href="">Electronics</a></li>
+                    <li><a href="">Home & Lifestyle</a></li>
+                    <li><a href="">Medicine</a></li>
+                    <li><a href="">Sports & Outdoor</a></li>
+                    <li><a href="">Baby's & Toys</a></li>
+                    <li><a href="">Groceries & Pets</a></li>
+                    <li><a href="">Health & Beauty</a></li>
+                </ul>
+            </nav>
+        </section>
+    </header>
---
+++ index.php
@@ -0,0 +1,224 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+        theme: {
+        extend: {
+            screens: {
+            'mouse': {'raw': '(hover: hover)'},
+            }
+        }
+        }
+    }
+    </script>
+
+    <title>Home Page</title>
+</head>
+<body>
+    <?php 
+    include 'header.php';
+    ?>
+    <main class="w-[80%] mx-auto">
+        <section class="mb-12">
+            <div class="flex mb-5">
+                <div class="w-4 mr-2 bg-[#DB4444]"></div>
+                <h3 class="text-[#DB4444] font-bold text-xl ">Categories</h3>
+            </div>
+            <h2 class="text-2xl font-bold mb-5">Browse By Category</h2>
+            <div class="grid justify-items-center grid-rows-2 grid-cols-3 gap-y-8 gap-x-9 lg:w-[60%] mx-auto">
+                <div class="p-1 mouse:hover:ring-2 mouse:hover:ring-[#DB4444]">
+                    <img src="./resources/electronics.png" alt="">
+                    <p class="text-sm font-semibold text-center mt-1">Electronics</p>
+                </div>
+                <div class="p-1 mouse:hover:ring-2 mouse:hover:ring-[#DB4444]" >
+                    <img src="./resources//living.png" alt="">
+                    <p class="text-sm font-semibold text-center mt-1">Furniture</p>
+                </div>
+                <div class="p-1 mouse:hover:ring-2 mouse:hover:ring-[#DB4444]">
+                    <img src="./resources/clothing.png" alt="">
+                    <p class="text-sm font-semibold text-center mt-1">Clothing</p>
+                </div>
+                <div class="p-1 mouse:hover:ring-2 mouse:hover:ring-[#DB4444]">
+                    <img src="./resources/sports-logo.png" alt="">
+                    <p class="text-sm font-semibold text-center mt-1">Sports</p>
+                </div>
+                <div class="p-1 mouse:hover:ring-2 mouse:hover:ring-[#DB4444]">
+                    <img src="./resources/groceries.png" alt="">
+                    <p class="text-sm font-semibold text-center mt-1">Groceries</p>
+                </div>
+                <div class="p-1 mouse:hover:ring-2 mouse:hover:ring-[#DB4444]">
+                    <img src="./resources/toys.png" alt="">
+                    <p class="text-sm font-semibold text-center mt-1">Toys</p>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="mb-12">
+            <div class="flex mb-5">
+                <div class="w-4 mr-2 bg-[#DB4444]"></div>
+                <h3 class="text-[#DB4444] font-bold text-xl ">Todays</h3>
+            </div>
+            <h2 class="text-2xl font-bold mb-5">Flash Sales</h2>         <!--Horizontal scroll-->
+            <div class="flex w-[100%] mx-auto justify-around">
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50 rounded-md">
+                        <img class="max-w-[100%] object-cover mx-auto" src="./resources//coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] md:w-6 md:h-6 h-4 w-4" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>The north coat</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50 rounded-md">
+                        <img class="max-w-[100%] object-cover mx-auto"  src="./resources//coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] h-4 w-4 md:w-6 md:h-6" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>Gucci duffle bag</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50">
+                        <img  class="max-w-[100%] object-cover mx-auto" src="./resources//coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] h-4 w-4 md:w-6 md:h-6" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>RGB liquid CPU Cooler</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50">
+                        <img class="max-w-[100%] object-cover mx-auto"  src="./resources/coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] h-4 w-4 md:w-6 md:h-6" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>Small BookSelf</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="mb-12">
+            <div class="flex mb-5">
+                <div class="w-4 mr-2 bg-[#DB4444]"></div>
+                <h3 class="text-[#DB4444] font-bold text-xl ">This Month</h3>
+            </div>
+            <h2 class="text-2xl font-bold mb-5">Best Selling Products</h2>         <!--Horizontal scroll-->
+            <div class="flex w-[100%] mx-auto justify-around">
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50 rounded-md">
+                        <img class="max-w-[100%] object-cover mx-auto" src="./resources//coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] md:w-6 md:h-6 h-4 w-4" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>The north coat</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50 rounded-md">
+                        <img class="max-w-[100%] object-cover mx-auto"  src="./resources//coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] h-4 w-4 md:w-6 md:h-6" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>Gucci duffle bag</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50">
+                        <img  class="max-w-[100%] object-cover mx-auto" src="./resources//coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] h-4 w-4 md:w-6 md:h-6" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>RGB liquid CPU Cooler</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+                <div class="w-[20%]">
+                    <div class="relative w-[100%] mx-auto bg-gray-50">
+                        <img class="max-w-[100%] object-cover mx-auto"  src="./resources/coat.png" alt="">
+                        <img class="absolute top-[3%] right-[2%] h-4 w-4 md:w-6 md:h-6" src="./resources/whitre-heart.png" alt="">
+                    </div>
+                    <h3>Small BookSelf</h3>
+                    <div class="flex">
+                        <p class="mr-4">$260</p>
+                        <p class="text-gray-500 line-through">$360</p>
+                    </div>
+                    <div>
+                        <div class="flex gap-x-1">
+                            <img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt=""><img src="./resources/fullStar.png" alt="">
+                        </div>
+                        <p>(65)</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include 'footer-service.php'?>
+    </main>
+
+<?php 
+include 'footer.php';
+?>
+    
+</body>
+</html>
